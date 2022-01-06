d20ed51e0fcccf71b1e24de20934951c578a40d8
Lee Seung Hun <negrat@naver.com>
2022-01-06 10:41:02 +0900

    버튼 클릭시 입력한 아이디 / 비번 저장

--- app/src/main/java/com/nepplus/apipractice_20220106/MainActivity.kt
+++ app/src/main/java/com/nepplus/apipractice_20220106/MainActivity.kt
@@ -3,12 +3,24 @@ package com.nepplus.apipractice_20220106
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.nepplus.apipractice_20220106.api.ServerAPI
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        ServerAPI.test()
+        btnLogin.setOnClickListener {
+
+//            입력한 이메일 / 비번 변수에 저장
+            val inputEmail = edtEmail.text.toString()
+            val inputPw = edtPassword.text.toString()
+
+//            서버 API의 로그인 기능에 활용
+
+
+
+        }
+
     }
 }
