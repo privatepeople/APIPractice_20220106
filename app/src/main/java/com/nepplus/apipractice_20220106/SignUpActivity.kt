0b466849b0b317a398a9efb5cfded8c8e0fc5f07
Lee Seung Hun <negrat@naver.com>
2022-01-06 11:43:57 +0900

    회원가입기능 2) 회원가입 버튼 이벤트 - 입력한 내용을 변수에 저장

--- app/src/main/java/com/nepplus/apipractice_20220106/SignUpActivity.kt
+++ app/src/main/java/com/nepplus/apipractice_20220106/SignUpActivity.kt
@@ -2,10 +2,22 @@ package com.nepplus.apipractice_20220106
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        btnSignUp.setOnClickListener {
+
+//            입력한 아이디 ~ 폰번 추출.
+            val inputEmail = edtEmail.text.toString()
+            val inputPw = edtPassword.text.toString()
+            val inputNickname = edtNickname.text.toString()
+            val inputPhoneNum = edtPhone.text.toString()
+
+        }
+
     }
 }
