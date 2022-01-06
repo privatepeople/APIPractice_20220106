0896206bc0eefe1a29ce0698763c20324fd44d9d
Lee Seung Hun <negrat@naver.com>
2022-01-06 10:36:29 +0900

    서버의 응답중, 기본적인 모양인     - code, message을 담아주는 클래스 생성

---
+++ app/src/main/java/com/nepplus/apipractice_20220106/api/APIList.kt
@@ -0,0 +1,25 @@
+package com.nepplus.apipractice_20220106.api
+
+import android.provider.ContactsContract
+import com.nepplus.apipractice_20220106.models.BasicResponse
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+interface APIList {
+
+//    api.gudoc.in 서버의 기능들을 접근하는 방법 (함수) 명시
+
+//    로그인
+
+    @FormUrlEncoded // @Field에 데이터를 담으려면 추가로 작성해야함
+    @POST("/user")
+    fun postRequestLogin(
+        @Field("email") email: String,
+        @Field("password") pw: String,
+    ) : Call<BasicResponse>  //  ( BasicResponse 형태의 응답을 받는 )  API 호출 기능을 만들어냄
+
+
+
+}
---
+++ app/src/main/java/com/nepplus/apipractice_20220106/models/BasicResponse.kt
@@ -0,0 +1,10 @@
+package com.nepplus.apipractice_20220106.models
+
+//  서버의 응답 (Response) 을 분석(Parsing) 해서 변수에 담아두는 역할
+
+class BasicResponse(
+    val code: Int,
+    val message: String,
+) {
+
+}
